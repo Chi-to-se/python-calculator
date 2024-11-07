699df15795f3e30b3537a88fdeebc96cc578396d
Chitipat Pangma <92365933+Chi-to-se@users.noreply.github.com>
2024-11-07 08:24:48 +0000

    Update files

--- calculator.py
+++ calculator.py
@@ -3,23 +3,54 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b # bug fixed
 
     def multiply(self, a, b):
-        result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
-        return result
+        result = 0 
+        if (b >= 0): # FIx bug
+            for i in range(b):
+                result = self.add(result, a)
+            return result
+        elif (a < 0 and b < 0):
+            for i in range(abs(b)):
+                result = self.add(result, abs(a))
+            return result
+        else:
+            for i in range(abs(b)):
+                result = self.add(result, -abs(a))
+            return result
+        
 
     def divide(self, a, b):
         result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
-        return result
+        if (a < 0 and b < 0): #bug fix
+            a,b = abs(a), abs(b)
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+            return (result)
+        elif (b < 0):
+            b = abs(b)
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+            return -abs(result)
+        elif (a < 0):
+            a = abs(a)
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+            return -abs(result)
+        elif (b == 0):
+            return 'Undefined'  
+        else:
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+            return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
--- test_calculator.py
+++ test_calculator.py
@@ -6,9 +6,63 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
+
+    # Add method
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
+    def test_negativeadd(self):
+        self.assertEqual(self.calc.add(-1, -2), -3)
+
+    # Subtract method
+    def test_positivesub(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+
+    def test_negativesub(self):
+        self.assertEqual(self.calc.subtract(1, 2), -1)
+
+    def test_zerosub(self):
+        self.assertEqual(self.calc.subtract(1, 1), 0)
+
+
+    # Multiply method
+    def test_positivemul(self):
+        self.assertEqual(self.calc.multiply(1, 2), 2)
+
+    def test_positive2mul(self):
+        self.assertEqual(self.calc.multiply(-1, -2), 2)
+
+    def test_negativemul(self):
+        self.assertEqual(self.calc.multiply(1, -2), -2)
+
+    def test_zeromul(self):
+        self.assertEqual(self.calc.multiply(0, 0), 0)    
+
+    # Div method
+    def test_positivediv(self):
+        self.assertEqual(self.calc.divide(10, 5), 2)
+
+    def test_positive2div(self):
+        self.assertEqual(self.calc.divide(-10, -5), 2)
+
+    def test_negativediv(self):
+        self.assertEqual(self.calc.divide(10, -5), -2)
+
+    def test_negative2div(self):
+        self.assertEqual(self.calc.divide(-10, 5), -2)        
+
+    def test_zerodiv(self):
+        self.assertEqual(self.calc.divide(0, 0), 'Undefined')
+
+
+    # Modulo method
+    def test_positivemod(self):
+        self.assertEqual(self.calc.modulo(10, 5), 0)
+
+    def test_positive2mod(self):
+        self.assertEqual(self.calc.modulo(10, 7), 3)
+    
+    
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
